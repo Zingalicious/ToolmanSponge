eccc6b6a6f19980fc8fd87527e263fa45f3344b2
zingten11 <zingten11@gmail.com>
2015-02-11 18:28:33 -0800

    Finally got Gradle working.

---
+++ src/main/java/us/zingalicio/toolman/Toolman.java
@@ -0,0 +1,11 @@
+package us.zingalicio.toolman;
+
+import org.spongepowered.api.plugin.Plugin;
+
+import us.zingalicio.cordstone.ZingPlugin;
+
+@Plugin(id = "toolman", name = "Toolman")
+public class Toolman extends ZingPlugin
+{
+	
+}
